2f4c5f2f6c3b4f90b82cf1dcd0a32ec396b0cc93
Chalvin Wiradhika <chalvinwz@gmail.com>
2023-07-07 14:59:28 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 async function getData() {
-	const url = process.env.VERCEL_URL
-		? `https://${process.env.VERCEL_URL}/api/`
+	const url = process.env.NEXT_PUBLIC_VERCEL_URL
+		? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/`
 		: 'http://localhost:3000/api/'
 
 	const res = await fetch(`${url}/hello`)
